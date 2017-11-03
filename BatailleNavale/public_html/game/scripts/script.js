053bdfcbf4f5dcded989a44388213c3fdae1964f
etudiant21 <etudiant21@DESKTOP-506UCDU>
2017-11-03 16:39:47 +0100

    Début de fonction de création de tableau

--- BatailleNavale/public_html/game/scripts/script.js
+++ BatailleNavale/public_html/game/scripts/script.js
@@ -1,7 +1,12 @@
 var tab1 = [];
 
-for (i=0; i<10; i++){
-    for (j=0; j<10; j++){
-        
+function createTab(){
+    console.log("prout");
+    for (i=65; i<=74; i++){
+        $("#tabPlayer").append("<tr>");
+        for (j=1; j<=10; j++){
+            $("#tabPlayer").append("<td id='"+String.fromCharCode(i)+"-"+j+"'></td>");
+        }
+        $("#tabPlayer").append("</tr>");
     }
 }
