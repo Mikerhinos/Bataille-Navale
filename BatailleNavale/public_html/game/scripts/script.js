e0ffdcd45fa2997b3486b947937aff461c131102
etudiant21 <etudiant21@DESKTOP-506UCDU>
2017-11-03 15:48:24 +0100

    Ajout fichier css et javascript Bootstrap et jQuery.

--- BatailleNavale/public_html/game/scripts/script.js
+++ BatailleNavale/public_html/game/scripts/script.js
@@ -1,7 +1,7 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+var tab1 = [];
 
-
+for (i=0; i<10; i++){
+    for (j=0; j<10; j++){
+        
+    }
+}
