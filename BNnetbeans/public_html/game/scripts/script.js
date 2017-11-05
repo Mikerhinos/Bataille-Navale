cf1545b431dfb16a3f50f8a7015db558c68e39a3
mikerhinos <mikerhinosfr@gmail.com>
2017-11-05 13:17:51 +0100

    Ajout des fichiers de curseur personnalisé, des numéros de colonne et lettre de ligne au tableau CPU. Visuellement améliorable on verra à la fin si on a le temps.

--- BNnetbeans/public_html/game/scripts/script.js
+++ BNnetbeans/public_html/game/scripts/script.js
@@ -13,19 +13,29 @@ $("document").ready(function createTabPlay(){
 
 // Génération du tableau CPU avec ids
 $("document").ready(function createTabCPU(){
-    for (i=65; i<=74; i++){
-        $(".tabCPU").append("<tr>");
+    $(".tabCPU").append("<tr><td id='celvide'></td><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td><td>9</td><td>10</td></tr>");// Header des numéros de colonnes
+    for (i=65; i<=74; i++){// Création des 10 lignes de jeu
+        $(".tabCPU").append("<tr id='"+String.fromCharCode(i)+"'><td>"+String.fromCharCode(i)+"</td>");// La première case de chaque ligne est la lettre
         for (j=1; j<=10; j++){
-            $(".tabCPU").append("<td id='"+String.fromCharCode(i)+"-"+j+"'><img class='img-responsive hoverLight' src='img/sea.gif'></td>");
+            $("#"+String.fromCharCode(i)).append("<td id='"+String.fromCharCode(i)+"-"+j+"'><img class='img-responsive hoverLight' src='img/sea.gif'></td>");// création des cellules avec id perso
         }
-        $(".tabCPU").append("</tr>");
+        $(".tabCPU").append("</tr>");// fin de ligne, ajout du tag de clôture
     }
-    $(".tabCPU").append("</tbody>");
 });
 
+// Fonction de récupération de l'id de la case cliquée
 $("document").ready(function tdClicked(){
     $("#tabCPUConst").on("click", "td", function() {
         console.log(this.id);
     });
 });
 
+//Fonction de placement des bateaux par le CPU
+$("document").ready(function placeShipsCPU(){
+    //TODO
+});
+
+//Fonction de placement des bateaux par le joueur
+$("document").ready(function placeShipsPLAYER(){
+    //TODO
+});
