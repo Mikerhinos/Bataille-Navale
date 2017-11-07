840bf29cf9960f8155221dcff652b7a2e7961121
Mikerhinos <mikerhinosfr@gmail.com>
2017-11-07 14:42:46 +0100

    optimisation des noms de sélecteurs.

--- game/scripts/script.js
+++ game/scripts/script.js
@@ -1,41 +1,44 @@
 // Génération du tableau Player avec ids
-$("document").ready(function createTabPlay(){
+var doc = $("document");
+var tabplayer = $(".tabPlayer");
+var tabcpu = $(".tabCPU");
+doc.ready(function createTabPlay(){
     for (i=65; i<=74; i++){
-        $(".tabPlayer").append("<tr>");
+        tabplayer.append("<tr>");
         for (j=1; j<=10; j++){
-            $(".tabPlayer").append("<td id='"+String.fromCharCode(i)+"-"+j+"'><img class='img-responsive' src='img/sea.gif'></td>");
+            tabplayer.append("<td id='"+String.fromCharCode(i)+"-"+j+"'><img class='img-responsive' src='img/sea.gif'></td>");
         }
-        $(".tabPlayer").append("</tr>");
+        tabplayer.append("</tr>");
     }
-    $(".tabPlayer").append("</tbody>");
+    tabplayer.append("</tbody>");
     
 });
 
 // Génération du tableau CPU avec ids
-$("document").ready(function createTabCPU(){
-    $(".tabCPU").append("<tr><td id='celvide'></td><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td><td>9</td><td>10</td></tr>");// Header des numéros de colonnes
+doc.ready(function createTabCPU(){
+    tabcpu.append("<tr><td id='celvide'></td><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td><td>7</td><td>8</td><td>9</td><td>10</td></tr>");// Header des numéros de colonnes
     for (i=65; i<=74; i++){// Création des 10 lignes de jeu
-        $(".tabCPU").append("<tr id='"+String.fromCharCode(i)+"'><td>"+String.fromCharCode(i)+"</td>");// La première case de chaque ligne est la lettre
+        tabcpu.append("<tr id='"+String.fromCharCode(i)+"'><td>"+String.fromCharCode(i)+"</td>");// La première case de chaque ligne est la lettre
         for (j=1; j<=10; j++){
             $("#"+String.fromCharCode(i)).append("<td id='"+String.fromCharCode(i)+"-"+j+"'><img class='img-responsive hoverLight' src='img/sea.gif'></td>");// création des cellules avec id perso
         }
-        $(".tabCPU").append("</tr>");// fin de ligne, ajout du tag de clôture
+        tabcpu.append("</tr>");// fin de ligne, ajout du tag de clôture
     }
 });
 
 // Fonction de récupération de l'id de la cellule cliquée
-$("document").ready(function tdClicked(){
+doc.ready(function tdClicked(){
     $("#tabCPUConst").on("click", "td", function() {
         console.log(this.id);
     });
 });
 
 //Fonction de placement des bateaux par le CPU
-$("document").ready(function placeShipsCPU(){
+doc.ready(function placeShipsCPU(){
     //TODO
 });
 
 //Fonction de placement des bateaux par le joueur
-$("document").ready(function placeShipsPLAYER(){
+doc.ready(function placeShipsPLAYER(){
     //TODO
 });
